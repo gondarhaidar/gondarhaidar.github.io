2480999bf6182e4a0e443719bc9000d5c531ee1a
gondar701 <gondarahmadhaidar@gmail.com>
2023-05-05 09:35:24 +0700

    Membuat collapse

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+//responsive navbar
 let x = document.getElementsByClassName("container")[0];
 let nav = document.getElementsByClassName("nav")[0];
 let width = x.clientWidth;
 nav.style.width = `${width}px`;
+//end
 
+//collpase
+let collap = document.querySelectorAll('.collap');
+let ket = document.querySelectorAll('.ket');
+for(let i=0; i < collap.length; i++){
+	collap[i].addEventListener('click', ()=>{
+		ket[i].classList.toggle('hide');
+	})
+}
+//emd
