ce53dafa190f599a1a90f4260650ddb505dc8bf8
gondar701 <gondarahmadhaidar@gmail.com>
2023-06-11 19:36:26 +0700

    menambahkan animasi scroll dengan js

--- profil/script.js
+++ profil/script.js
@@ -1,4 +1,19 @@
 let ul = document.querySelector('.navbar');
 let nav = ()=>{
-	ul.classList.toggle('muncul');
+	ul.classList.add('open');
 }
+let svg = document.querySelector('.svg');
+svg.onclick = ()=>{
+	ul.classList.remove('open');
+}
+
+let elements = document.querySelectorAll('.hide');
+window.addEventListener('scroll', () => {
+  elements.forEach(e => {
+    if (e.getBoundingClientRect().top < window.innerHeight - 180) {
+      e.classList.add('muncul');
+    }else{
+    	e.classList.remove('muncul');
+    }
+  });
+});
