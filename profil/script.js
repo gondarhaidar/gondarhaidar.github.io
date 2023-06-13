37d11ef995839bce8debc6b4245e66233a645388
gondar701 <gondarahmadhaidar@gmail.com>
2023-06-13 18:47:45 +0700

    add

--- profil/script.js
+++ profil/script.js
@@ -6,14 +6,23 @@ let svg = document.querySelector('.svg');
 svg.onclick = ()=>{
 	ul.classList.remove('open');
 }
-
+let li = document.querySelectorAll('.skils ul li');
+li.forEach(el=>{
+  el.classList.add('hide');
+})
 let elements = document.querySelectorAll('.hide');
+let skil = document.querySelector('.skils');
 window.addEventListener('scroll', () => {
   elements.forEach(e => {
     if (e.getBoundingClientRect().top < window.innerHeight - 180) {
       e.classList.add('muncul');
     }else{
     	e.classList.remove('muncul');
+    }
+    if(skil.getBoundingClientRect().top < window.innerHeight - 100){
+      skil.classList.add('satu');
+    }else{
+      skil.classList.remove('add');
     }
   });
 });
