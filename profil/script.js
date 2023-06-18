223b0b66c37aba4a8c4e2c0baaaa1c70df40b9e3
gondar701 <gondarahmadhaidar@gmail.com>
2023-06-18 14:08:06 +0700

    menambahkan class aktif

--- profil/script.js
+++ profil/script.js
@@ -12,20 +12,59 @@ let li = document.querySelectorAll('.skils ul li');
 li.forEach(el=>{
   el.classList.add('hide');
 })
-
+let profil = document.querySelector('.profil');
 let elements = document.querySelectorAll('.hide');
 let skil = document.querySelector('.skils');
 window.addEventListener('scroll', () => {
+
+
+
+/*********************************************************/
     if(skil.getBoundingClientRect().top < window.innerHeight - 100){
       skil.classList.add('satu');
     }else{
       skil.classList.remove('satu');
     }
-  elements.forEach(e => {
+
+
+
+
+
+/*********************************************************/
+
+  elements.forEach((e, i) => {
     if (e.getBoundingClientRect().top < window.innerHeight - 180) {
       e.classList.add('muncul');
     }else{
     	e.classList.remove('muncul');
     }
   });
+
+
+
+
+/*********************************************************/
+  let bar = document.querySelectorAll('.bar');
+  bar.forEach((e, i)=>{
+    if(e.getBoundingClientRect().top < window.innerHeight - 150 && e.getBoundingClientRect().bottom > 150){
+      document.querySelectorAll('.menu li')[i].classList.add('aktif');
+      document.querySelectorAll('.navbar li')[i].classList.add('aktif');
+    }else{
+      document.querySelectorAll('.menu li')[i].classList.remove('aktif');
+      document.querySelectorAll('.navbar li')[i].classList.remove('aktif');
+    }
+  });
+
+
 });
+
+
+// let klikBar = document.querySelectorAll('.menu li');
+// let klikNav = document.querySelectorAll('.navbar li');
+// let target = document.querySelectorAll('.bar');
+// klikBar.forEach((e, i) => {
+//     e.addEventListener('click', ()=>{
+//       target[i].getBoundingClientRect().top = 100;
+//       console.log('oke')
+//     });
+// });
